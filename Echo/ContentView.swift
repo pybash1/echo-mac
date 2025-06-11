09e8ea6ce8c27dfbcb1a8424861160f8159b91ed
PyBash <67195650+pybash1@users.noreply.github.com>
2025-06-11 15:17:44 +0530

    Initial Commit

---
+++ Echo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Echo
+//
+//  Created by Ananjan Mitra on 11/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Echo/EchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoApp.swift
+//  Echo
+//
+//  Created by Ananjan Mitra on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct EchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
